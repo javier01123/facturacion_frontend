99c75b76a86ad62c84543ac63710e7950ba76b1d
Javier Andrade <javier01123@gmail.com>
2020-06-19 16:38:35 -0600

    fixed test

--- src/App.js
+++ src/App.js
@@ -1,29 +1,62 @@
 import React from "react";
 import { Route, Switch, Router } from "react-router";
-import { Link } from "react-router-dom";
-import ErrorBoundary from  "./components/ErrorHandlers/ErrorBoundary/ErrorBoundary";
 import Home from "./containers/Home/Home";
 import Login from "./containers/PantallaLogin/PantallaLogin";
 import { createBrowserHistory } from "history";
+import { createStore, compose, applyMiddleware } from "redux";
+import reducer from "./store/reducers/reducer";
+import { Provider } from "react-redux";
+import createSagaMiddleware from "redux-saga";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import CustomSpinner from "./components/CustomSpinner/CustomSpinner";
+import {
+  watchEmpresasReload,
+  watchEmpresaChanged,
+} from "./store/sagas/watcher";
 import "antd/dist/antd.css";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const sagaMiddleware = createSagaMiddleware();
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+// const rootReducer = combineReducers({reducer});
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const store = createStore(
+  persistedReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
+
+const persistor = persistStore(store);
+
+sagaMiddleware.run(watchEmpresasReload);
+sagaMiddleware.run(watchEmpresaChanged);
 
 const App = () => {
-
   const history = createBrowserHistory();
 
   return (
-    <Router history={history}>
-    <Switch>
-      <Route path="/login">
-        <Login />
-      </Route>
+    <Provider store={store}>
+      <PersistGate loading={<CustomSpinner />} persistor={persistor}>
+        <Router history={history}>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
 
-      <Route path="/">
-        <Home />
-      </Route>
-    </Switch>
-    </Router>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,50 +1,17 @@
+
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore, compose, combineReducers, applyMiddleware } from "redux";
-import reducer from "./store/reducers/reducer";
-import { Provider } from "react-redux";
-import {
-  watchEmpresasReload,
-  watchEmpresaChanged,
-} from "./store/sagas/watcher";
-import createSagaMiddleware from "redux-saga";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import { PersistGate } from "redux-persist/lib/integration/react";
-import CustomSpinner from "./components/CustomSpinner/CustomSpinner";
+import "./index.css";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const sagaMiddleware = createSagaMiddleware();
-
-const persistConfig = {
-  key: "root",
-  storage,
-};
-
-// const rootReducer = combineReducers({reducer});
-const persistedReducer = persistReducer(persistConfig, reducer);
-
-const store = createStore(
-  persistedReducer,
-  composeEnhancers(applyMiddleware(sagaMiddleware))
-);
-
-const persistor = persistStore(store);
-
-sagaMiddleware.run(watchEmpresasReload);
-sagaMiddleware.run(watchEmpresaChanged);
 
 ReactDOM.render(
   // <React.StrictMode>
 
-  <Provider store={store}>
-    <PersistGate loading={<CustomSpinner />} persistor={persistor}>
+ 
       <App />
-    </PersistGate>
-  </Provider>,
+  ,
   // </React.StrictMode>
   document.getElementById("root")
 );
